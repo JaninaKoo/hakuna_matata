402713696cb828df3ce964f7ee3e331da42118dc
Ana Ivan <ana.ivan.m@gmail.com>
2018-11-12 14:19:56 +0100

    Basic structure

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
-  get 'beaches/index'
-  get 'beaches/new'
-  get 'beaches/create'
-  get 'beaches/show'
-  get 'beaches/destroy'
-  get 'bookings/index'
-  get 'bookings/create'
-  get 'bookings/show'
-  get 'bookings/destroy'
+  get 'beaches/:id', to: "beaches#show"
+  get 'beaches/new', to: "beaches#new"
+  post 'beaches', to: "beaches#create"
+
+  get 'bookings', to: "bookings#index"
+  post 'bookings', to: "bookings#create"
+  delete 'bookings/:id', to: "bookings#destroy"
+
+  get 'my_beaches', to: "beaches#my_beaches"
+
   devise_for :users
-  root to: 'home#index'
+
+  root to: 'beaches#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
