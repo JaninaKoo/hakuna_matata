77b65f13242a9babd00f214eed529393fa53fd36
Adline Isiji <adlyne@gmail.com>
2018-11-14 14:56:38 +0100

    Redirected to listing of beaches after log-in and a go back button on  the booking page

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,4 +3,9 @@ class DashboardController < ApplicationController
     @user = current_user
     @bookings = Booking.all
   end
+
+# def destroy
+#   @Booking.destroy
+#   redirect_to bookings_url, notice: 'Restaurant was successfully destroyed.'
+# end
 end
