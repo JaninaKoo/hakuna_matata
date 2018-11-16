959764176ea8e0d089758fd8edbd5938d27b2af0
Ana Ivan <ana.ivan.m@gmail.com>
2018-11-16 12:27:11 +0100

    It will redirect you to the last page after you sign in

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,21 +17,21 @@ user = User.create(email: 'steve@gmail.com', password: '123456')
 puts "Adding beaches..."
 
 beaches = [
-  { user: user, name: 'Aloha', location: 'Europe', description: 'Beautiful beach', price: 100, photo: Rails.root.join("app/assets/images/beach_1.jpeg").open },
-  { user: user, name: 'Kuala', location: 'Australia', description: 'Amazing place', price: 150, photo: Rails.root.join("app/assets/images/beach_2.jpeg").open },
-  { user: user, name: 'Santos', location: 'Australia', description: 'Beautiful beach', price: 150, photo: Rails.root.join("app/assets/images/beach_3.jpeg").open },
-  { user: user, name: 'Kirailia', location: 'Europe', description: 'Amazing place', price: 150, photo: Rails.root.join("app/assets/images/beach_4.jpeg").open },
-  { user: user, name: 'Tranquility', location: 'Australia', description: 'Amazing place', price: 2000, photo: Rails.root.join("app/assets/images/beach_5.jpeg").open },
+  { user: user, name: 'Aloha', location: 'Patong', description: 'Beautiful beach', price: 100, photo: Rails.root.join("app/assets/images/beach_1.jpeg").open },
+  { user: user, name: 'Kuala', location: 'Kuta', description: 'Amazing place', price: 150, photo: Rails.root.join("app/assets/images/beach_2.jpeg").open },
+  { user: user, name: 'Santos', location: 'Siaton', description: 'Beautiful beach', price: 150, photo: Rails.root.join("app/assets/images/beach_3.jpeg").open },
+  { user: user, name: 'Kirailia', location: 'Mambajao', description: 'Amazing place', price: 150, photo: Rails.root.join("app/assets/images/beach_4.jpeg").open },
+  { user: user, name: 'Tranquility', location: 'Ormoc', description: 'Amazing place', price: 2000, photo: Rails.root.join("app/assets/images/beach_5.jpeg").open },
   { user: user, name: 'Oasis', location: 'Asia', description: 'Beautiful beach', price: 2000, photo: Rails.root.join("app/assets/images/beach_6.jpeg").open },
-  { user: user, name: 'Neptune', location: 'Europe', description: 'Amazing place', price: 566, photo: Rails.root.join("app/assets/images/beach_7.jpeg").open },
-  { user: user, name: 'Atlantis', location: 'Europe', description: '5-star treatment', price: 987, photo: Rails.root.join("app/assets/images/beach_8.jpeg").open },
-  { user: user, name: 'Boa', location: 'Asia', description: '5-star treatment', price: 800, photo: Rails.root.join("app/assets/images/beach_10.jpeg").open },
-  { user: user, name: 'Seven Seas', location: 'Asia', description: 'Beautiful beach', price: 2000, photo: Rails.root.join("app/assets/images/beach_11.jpeg").open },
-  { user: user, name: 'Love island', location: 'Australia', description: 'Beautiful beach', price: 800, photo: Rails.root.join("app/assets/images/beach_12.jpeg").open },
-  { user: user, name: 'Dreams', location: 'Asia', description: '5-star treatment', price: 800, photo: Rails.root.join("app/assets/images/beach_13.jpeg").open },
-  { user: user, name: 'Pina', location: 'Asia', description: '5-star treatment', price: 800, photo: Rails.root.join("app/assets/images/beach_14.jpeg").open },
-  { user: user, name: 'Exclusive', location: 'Asia', description: '5-star treatment', price: 800, photo: Rails.root.join("app/assets/images/beach_15.jpeg").open },
-  { user: user, name: 'Mauria', location: 'Asia', description: '5-star treatment', price: 800, photo: Rails.root.join("app/assets/images/beach_16.jpeg").open },
+  { user: user, name: 'Neptune', location: 'Mallorca', description: 'Amazing place', price: 566, photo: Rails.root.join("app/assets/images/beach_7.jpeg").open },
+  { user: user, name: 'Atlantis', location: 'Algarve', description: '5-star treatment', price: 987, photo: Rails.root.join("app/assets/images/beach_8.jpeg").open },
+  { user: user, name: 'Boa', location: 'Goa', description: '5-star treatment', price: 800, photo: Rails.root.join("app/assets/images/beach_10.jpeg").open },
+  { user: user, name: 'Seven Seas', location: 'Lagos', description: 'Beautiful beach', price: 2000, photo: Rails.root.join("app/assets/images/beach_11.jpeg").open },
+  { user: user, name: 'Love island', location: 'Salento', description: 'Beautiful beach', price: 800, photo: Rails.root.join("app/assets/images/beach_12.jpeg").open },
+  { user: user, name: 'Dreams', location: 'Thasos', description: '5-star treatment', price: 800, photo: Rails.root.join("app/assets/images/beach_13.jpeg").open },
+  { user: user, name: 'Pina', location: 'Milos', description: '5-star treatment', price: 800, photo: Rails.root.join("app/assets/images/beach_14.jpeg").open },
+  { user: user, name: 'Exclusive', location: 'Ko Samui', description: '5-star treatment', price: 800, photo: Rails.root.join("app/assets/images/beach_15.jpeg").open },
+  { user: user, name: 'Mauria', location: 'Ydra', description: '5-star treatment', price: 800, photo: Rails.root.join("app/assets/images/beach_16.jpeg").open },
 ]
 
 beaches.each do |beach|
